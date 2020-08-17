7aeabcd16afe4c19f30d6ca2cc24bebece36eaf7
Bright Jiang <bright.jiang@amd.com>
2020-08-17 14:39:38 +0800

    fix slagon

--- _site/build.sh
+++ _site/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 bundle  exec jekyll serve
+echo "will build html"
+sleep 1
+echo "will build html"
+sleep 1
+echo "will build html"
 sleep 1
 echo "will build html"
 bundle  exec jekyll build
